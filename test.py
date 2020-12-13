782c54eb5c9a1ee55d2b28b2ebf14812351e74f5
RUnroe <ryanunroe@gmail.com>
2020-12-13 14:08:25 -0700

    Send post request to custom API

--- test.py
+++ test.py
@@ -1,18 +1,25 @@
-import speech_recognition as sr
-import pyttsx3 as tts
+#import speech_recognition as sr
+#import pyttsx3 as tts
+import requests
 
-r = sr.Recognizer()
-out = tts.init()
+#r = sr.Recognizer()
+#out = tts.init()
+#mic = sr.Microphone()
 
-mic = sr.Microphone()
+baseURL = 'http://192.168.1.193:8060/'
 
-with mic as source:
-    while(True):
-        r.adjust_for_ambient_noise(source)
-        audio = r.listen(source)
-        words = r.recognize_google(audio)
-        print(words)
-        if 'hi' in words or 'hello' in words:
-            out.say('fuck off mate')
-        elif 'stop' in words:
-            break
+#x = requests.post('http://192.168.1.182:8060/keypress/powerOn', data={})
+x = requests.post(baseURL + 'power/off', data={});
+print(x.text)
+print(x)
+print(x.request)
+#with mic as source:
+#    while(True):
+#        r.adjust_for_ambient_noise(source)
+#        audio = r.listen(source)
+#        words = r.recognize_google(audio)
+#        print(words)
+#        if 'hi' in words or 'hello' in words:
+#            out.say('fuck off mate')
+#        elif 'stop' in words:
+#            break
